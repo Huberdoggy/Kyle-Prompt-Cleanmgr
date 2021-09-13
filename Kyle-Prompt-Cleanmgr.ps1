903f9fa30fa1ad5c0c56cab391a22ff57e661e4a
Huberdoggy <huberdoggy@gmail.com>
2021-09-13 08:47:51 -0500

    added additional pauses. Set -NoProfile and -NoExit flags in SchTasks. Added Read-Host prompt at the end to force affirmative for shutdown. Removed '/f' flag since it's implied by '/t'

--- Kyle-Prompt-Cleanmgr.ps1
+++ Kyle-Prompt-Cleanmgr.ps1
@@ -17,6 +17,8 @@ function Should_Run_Cleaner()
       $cleanup = $false
       $get_input = Read-Host -Prompt "Would you like to run your weekly Cleanmgr settings at this time (y/n)? "
       if ($get_input -like "y") {
+           Write-Host "Success!" -fore Green
+           Start-Sleep -Seconds 3
            Start-Process -FilePath "$env:windir\System32\cleanmgr.exe" -ArgumentList "/d C /VERYLOWDISK" -Wait
            if ($?) {
            $cleanup = $true
@@ -34,8 +36,11 @@ $outcome = Should_Run_Cleaner
 
 if ($outcome) {
     Write-Host "Status of cleaning command execution: $outcome" -fore Green
-    Write-Host "[+] System will now reboot to finish cleaning. Thanks Kyle." -fore Cyan
-    shutdown.exe /r /f /t 5
+    Write-Host "[+] System will prompt for reboot to finish cleaning in 5 seconds..." -fore Cyan
+    Start-Sleep -Seconds 5
+    Read-Host "Press any key to restart the system now" # set this to delay execution, Task is set with -NoExit so window will stay open
+    # until closed manually by shutdown process. '/f' flag is implied when '/t' is specified. 
+    shutdown /r /t 5
 }
 else {
     Write-Host "`n[!] Either you quit the program or an error occured.`nPlease manually run Disk Cleanup." -fore Red
